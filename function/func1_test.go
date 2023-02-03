8c3f1b5e4d3f2d8ea864e6191edf2e14f0c057cd
LTC <>
2023-02-03 18:57:11 +0800

    方法作为函数参数的进一步测试

--- function/func1_test.go
+++ function/func1_test.go
@@ -30,3 +30,20 @@ func TestReturnMethod(t *testing.T) {
 	//fmt.Println(f(1))
 	fmt.Println(reflect.TypeOf(f))
 }
+
+func FuncAsParams(f func(times int)) {
+	fmt.Println(reflect.TypeOf(f))
+	fmt.Println(reflect.ValueOf(f))
+	f(2)
+}
+
+func TestMethodAsParams(t *testing.T) {
+	s := Student{Name: "zhangsan"}
+
+	// 经测试，结构体的方法也可作为函数的参数传入
+	FuncAsParams(s.PrintName)
+
+	// 会发现，将方法传入时，方法相当于绑定了结构体
+	s = Student{Name: "lisi"}
+	FuncAsParams(s.PrintName)
+}
